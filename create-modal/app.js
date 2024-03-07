c4f113eef234dec85fb5b4425f6794343b9bb4ee
ralph2308s <ralph2308s@gmail.com>
2024-03-07 07:22:53 +0700

    update app.js create-modal

--- create-modal/app.js
+++ create-modal/app.js
@@ -1,9 +1,5 @@
 const $ = document.querySelector.bind(document);
 
-function toggleModal() {
-  $(".modal").classList.toggle("hide");
-}
-
 $(".open-modal-btn").addEventListener("click", toggleModal);
 $(".modal__header i").addEventListener("click", toggleModal);
 $(".modal__footer button").addEventListener("click", toggleModal);
@@ -11,3 +7,7 @@ $(".modal__footer button").addEventListener("click", toggleModal);
 $(".modal").addEventListener("click", (e) => {
   if (e.target == e.currentTarget) toggleModal();
 });
+
+function toggleModal() {
+  $(".modal").classList.toggle("hide");
+}
